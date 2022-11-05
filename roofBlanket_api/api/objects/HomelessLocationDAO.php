e127c8de8e5e8aaac074f0a5b7d09caf1ce5ac69
Ren Kee <renkeerenkee@gmail.com>
2022-11-05 23:50:56 +0800

    Include api call for homeless location by id

--- roofBlanket_api/api/objects/HomelessLocationDAO.php
+++ roofBlanket_api/api/objects/HomelessLocationDAO.php
@@ -46,5 +46,49 @@
 
             return $result_arr;
         }
+
+        public function retrieve_homelesslocation_by_id($id){
+            $conn_manager = new Database();
+            $pdo = $conn_manager->getConnection();
+            
+            $sql = "select * from homelesslocation where id=:id";
+
+            
+            $stmt = $pdo->prepare($sql);
+            $stmt->bindParam(":id",$id,PDO::PARAM_INT);
+            $stmt->execute();
+            
+            $result_arr = null;
+
+            $num = $stmt->rowCount();
+            if($num > 0) {
+
+                // products array
+                $result_arr = array();
+                // []
+                $result_arr["records"] = array();
+
+                while( $row = $stmt->fetch(PDO::FETCH_ASSOC) ) {
+                    // extract row
+                    // this will make $row['name'] to
+                    // just $name only
+                    // converts a dictionary key to variable name and value to variable value
+                    extract($row);
+
+                    $homeless_loc = array(
+                        "id" => $id,
+                        "latitude"  => $latitude,
+                        "longitude" => $longitude,
+                    );
+
+                    array_push($result_arr["records"], $homeless_loc);
+                }
+            }
+
+            $stmt = null;
+            $pdo = null;
+
+            return $result_arr;
+        }
     }
 ?>
